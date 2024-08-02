6441f0f30cb0c35eaca2f2975c955e1e394072be
filispeen <illayfilisp@gmail.com>
2024-08-03 00:40:41 +0300

    First upload

---
+++ src/preload.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const el = document.getElementById('clickThroughElement')
+    el.addEventListener('mouseenter', () => {
+      ipcRenderer.send('set-ignore-mouse-events', true, { forward: true })
+    })
+    el.addEventListener('mouseleave', () => {
+      ipcRenderer.send('set-ignore-mouse-events', false)
+    })
+  })
