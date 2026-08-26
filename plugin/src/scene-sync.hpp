900d2d011a72cc3c081baae31ed37e3149e48a10
filispeen <illayfilisp@gmail.com>
2026-08-26 14:07:45 +0300

    Revert "Fixed show_onscreen filter not actually skipping render on target source"
    This reverts commit e71a3c36bb26858ba04c9b7382d776bc7d80e87f.

--- plugin/src/scene-sync.hpp
+++ plugin/src/scene-sync.hpp
@@ -8,6 +8,5 @@ void start();
 void stop();
 
 void notify_filter_changed(obs_source_t *filter_context);
-void apply_show_onscreen_visibility(obs_source_t *filter_context, bool show_onscreen);
 
 } // namespace scene_sync
